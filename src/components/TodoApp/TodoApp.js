8287b0c20a6932940ef195b246e629a3ee8c69bb
dilu799 <dhilshad799@gmail.com>
2021-05-12 23:34:57 +0530

    todo add and delete

--- src/components/TodoApp/TodoApp.js
+++ src/components/TodoApp/TodoApp.js
@@ -2,15 +2,57 @@ import React, { Component } from 'react'
 import './TodoApp.css'
 
 export default class TodoApp extends Component {
+    state = {
+        input: '',
+        items: []
+    }
+    handleChange = (event) => {
+        this.setState({
+            input: event.target.value
+        })
+    }
+
+    storeItems = (e) => {
+        e.preventDefault();
+        const { input } = this.state;
+
+        this.setState({
+            items: [...this.state.items, input],
+            input: ''
+        })
+    }
+
+    deleteItem = (index) => {
+        this.setState({
+            items: this.state.items.filter((data, key) => key !== index)
+        })
+    }
+
+    editItem = (index)=>{
+        let wish = prompt('Edit your wish!',this.state.items.filter((data,key)=>key === index))
+
+        if(wish != null){
+            this.setState({
+                items: [...this.state.items,this.state.items[index] = wish]
+            })
+        }else{
+            
+        }
+    }
+
     render() {
+        const { input, items } = this.state;
+        console.log(items);
         return (
             <div className='todo-container'>
-                <form className='input-section'>
-                <h1>Todo App</h1>
-                    <input type="text" placeholder='Enter your wish' />
+                <form className='input-section' onSubmit={this.storeItems}>
+                    <h1>Todo App</h1>
+                    <input type="text" onChange={this.handleChange} value={input} placeholder='Enter your wish' />
                 </form>
                 <ul>
-                    <li>Items <i className='fas fa-trash-alt'></i></li>
+                    {items.map((data, index) => (
+                        <li key={index}>{data}<i className="fas fa-edit" onClick={() => this.editItem(index)}></i><i className='fas fa-trash-alt' onClick={() => this.deleteItem(index)}></i></li>
+                    ))}
                 </ul>
             </div>
         )
